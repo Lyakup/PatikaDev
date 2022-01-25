1f7435f905a9e178864f27cb09bca5640b74e7b7
Yakup Layık <yakuplayik@hotmail.com>
2022-01-25 08:42:29 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PatikaDev_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, İsminizi Giriniz ?");
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldin");
+            Console.WriteLine(inputName);
+        }
+    }
+}
